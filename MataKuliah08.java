5cb0aebfd01080e176677333f922a681ba97656f
kamilakhairunnisa <erinazkk@gmail.com>
2026-02-20 10:14:46 +0700

    Matakuliah08

---
+++ MataKuliah08.java
@@ -0,0 +1,42 @@
+public class MataKuliah08 {
+
+    String kodeMK;
+    String nama;
+    int sks;
+    int jumlahJam;
+
+    MataKuliah08() { }
+
+    MataKuliah08(String kodeMK, String nama, int sks, int jumlahJam) {
+        this.kodeMK = kodeMK;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Kode MK     : " + kodeMK);
+        System.out.println("Nama MK     : " + nama);
+        System.out.println("SKS         : " + sks);
+        System.out.println("Jumlah Jam  : " + jumlahJam);
+    }
+
+    void ubahSKS(int sksBaru) {
+        sks = sksBaru;
+        System.out.println("SKS berhasil diubah menjadi " + sks);
+    }
+
+    void tambahJam(int jam) {
+        jumlahJam += jam;
+        System.out.println("Jumlah jam setelah ditambah: " + jumlahJam);
+    }
+
+    void kurangiJam(int jam) {
+        if (jumlahJam >= jam) {
+            jumlahJam -= jam;
+            System.out.println("Jumlah jam setelah dikurangi: " + jumlahJam);
+        } else {
+            System.out.println("Pengurangan tidak dapat dilakukan. Jam tidak mencukupi.");
+        }
+    }
+}
