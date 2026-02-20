c7602ba766bbef3bbe1457d629450ccaf89b71ef
kamilakhairunnisa <erinazkk@gmail.com>
2026-02-20 08:50:57 +0700

    Mahasiswa08-Modifikasi(no.4)

--- Mahasiswa08.java
+++ Mahasiswa08.java
@@ -18,8 +18,12 @@ public class Mahasiswa08 {
     }
 
     void updateIpk(double ipkBaru) {
+        if(ipkBaru>=0.0 && ipkBaru<4.0){
         ipk = ipkBaru;
-    } 
+    }   else{
+        System.out.println("IPK Tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
 
     String nilaiKerja() {
         if (ipk >= 3.5) {
