46f1b2d49b5a0d2a365f32f2c58f620645fc1e92
kamilakhairunnisa <erinazkk@gmail.com>
2026-02-20 09:21:43 +0700

    Mahasiswamain08(percobaan3)

---
+++ MahasiswaMain08.java
@@ -0,0 +1,19 @@
+public class MahasiswaMain08 {
+    public static void main(String[] args) {
+        
+        Mahasiswa08 mhs1 = new Mahasiswa08();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+
+        Mahasiswa08 mhs2 = new Mahasiswa08("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+    }
+}
