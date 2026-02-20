676143ff0b9ae7ba6533e518fea0d8cd3d37111d
kamilakhairunnisa <erinazkk@gmail.com>
2026-02-20 10:53:44 +0700

    MataKuliahmain08

---
+++ MataKuliahMain08.java
@@ -0,0 +1,22 @@
+public class MataKuliahMain08 {
+    public static void main(String[] args) {
+
+        MataKuliah08 mk1 = new MataKuliah08();
+        mk1.kodeMK = "ASD08";
+        mk1.nama = "Algoritma dan Struktur Data";
+        mk1.sks = 3;
+        mk1.jumlahJam = 6;
+
+        mk1.tampilInformasi();
+        mk1.ubahSKS(4);
+        mk1.tambahJam(2);
+        mk1.kurangiJam(3);
+        
+
+        MataKuliah08 mk2 = new MataKuliah08("BD08", "Basis Data", 2, 4);
+
+        mk2.tampilInformasi();
+        mk2.tambahJam(1);
+        mk2.kurangiJam(10); 
+    }
+}
